d9c1310c1d02dda68a9900e2c7babf76668b180a
Guix <88253846+xGuix@users.noreply.github.com>
2022-07-26 21:03:26 +0200

    Setup for Docker deploy

--- assessment/src/main/java/com/mediscreen/assessment/proxy/PatientProxy.java
+++ assessment/src/main/java/com/mediscreen/assessment/proxy/PatientProxy.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
-@FeignClient(value = "patient", url = "localhost:8080" /*"${mediscreen.patientUrl}"*/)
+@FeignClient(value = "patient", url = "${mediscreen.patientUrl}")
 public interface PatientProxy {
     @GetMapping("/api/patient")
     PatientDto patientById(@RequestParam Long id);
